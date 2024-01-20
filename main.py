ca5be1190108c45e9fe82251ec835d6d8dfb2380
unknown <md.ridgeway.work@gmail.com>
2024-01-20 13:34:37 -0600

    Modify Games Dict

--- main.py
+++ main.py
@@ -27,7 +27,23 @@ for year in range(2015, 2023):
     response = betting_api.get_lines(year=year)
     lines = [*lines, *response]
 
-games = [g for g in games if g.home_division != 'fcs' and g.away_division != 'fcs' and g.home_points is not None and g.away_points is not None]
-len(games)
+    games = [g for g in games if g.home_division != 'fcs' and g.away_division != 'fcs' and g.home_points is not None and g.away_points is not None]
+    len(games)
+
+games = [
+    dict(
+        id = g.id,
+        year = g.season,
+        week = g.week,
+        neutral_site = g.neutral_site,
+        home_team = g.home_team,
+        home_conference = g.home_conference,
+        home_points = g.home_points,
+        home_elo = g.home_pregame_elo,
+        away_team = g.away_team,
+        away_conference = g.away_conference,
+        away_points = g.away_points,
+        away_elo = g.away_pregame_elo
+    ) for g in games]
 
 print(games[0])
